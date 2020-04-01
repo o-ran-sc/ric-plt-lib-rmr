a1a5420f5468717130b182e197374f25f32ec76d
E. Scott Daniels <daniels@research.att.com>
2020-04-01 12:39:47 -0400

    Fix potential nil ptr seg fault and CI test issue
    This change corrects two problems that the "dockerless"
    CI build process was encountering:
    	- Include and link paths set in the CMake config
    	  were not always honoured by the application test
    	  scripts
    
    	- Building packges to install in the test environment
    	  as a non-root user was causing non-standard install
    	  prefixes to be added to the packages.
    
    This change also corrects a typo in the NNG module which
    potentially could have allowed a nil pointer to be
    dereferenced. This bug was discovered while makeing the
    changes for the CI jobs.
    
    Issue-ID: RIC-303
    
    Signed-off-by: E. Scott Daniels <daniels@research.att.com>
    Change-Id: I30150950dd186e72f8c822ec3fbe5b07369cb4ce

--- test/app_test/run_rts_test.sh
+++ test/app_test/run_rts_test.sh
@@ -19,71 +19,51 @@
 #
 
 # ---------------------------------------------------------------------------------
-#	Mnemonic:	run_multi_test.ksh
+#	Mnemonic:	run_rts_test.ksh
 #	Abstract:	This is a simple script to set up and run the basic send/receive
 #				processes for some library validation on top of nng. This
-#				particular tests starts several receivers and creates a route table
-#				which causes messages to be sent to all receivers in parallel
-#				(forcing message cloning internally in RMr).
-#				It should be possible to clone the repo, switch to this directory
-#				and execute  'ksh run -B'  which will build RMr, make the sender and
-#				recevier then  run the basic test.
+#				particular test starts several senders and one receiver. All messages
+#				go to the receiver and an ack is sent back to the sending process.
+#				Each sender puts a tag into the message allowing it to verify that
+#				all messages received were 'acks' to the ones it sent, and that no
+#				rts messages were routed to the wrong sender.
 #
 #				Example command line:
-#					ksh ./run_multi_test.ksh		# default 10 messages at 1 msg/sec
-#					ksh ./run_multi_test.ksh -d 100 -n 10000 # send 10k messages with 100ms delay between
 #
-#	Date:		24 April 2019
+#	Date:		15 May 2019
 #	Author:		E. Scott Daniels
 # ---------------------------------------------------------------------------------
 
 
 # The sender and receivers are run asynch. Their exit statuses are captured in a
-# file in order for the 'main' to pick them up easily. For the multi test the 
-# async connect must be DISABLED because in some environments (um, jenkins) the
-# session connect time lags enough that the first message can be dropped silently.
-# It doesn't happen all of the time, but frequently enough to be annoying. 
+# file in order for the 'main' to pick them up easily.
+# $1 is the instance so we can keep logs separate.
 #
 function run_sender {
-	RMR_ASYNC_CONN=0 ./sender${si} $nmsg $delay
-	echo $? >/tmp/PID$$.src		# must communicate state back via file b/c asynch
+	export RMR_RTG_SVC=$(( 9991 + $1 ))
+	port=$((  43080 + $1 ))
+	./sender${si} $nmsg $delay 5:6 $port
+	echo $? >/tmp/PID$$.$1.src		# must communicate state back via file b/c asynch
 }
 
 # $1 is the instance so we can keep logs separate
 function run_rcvr {
 	typeset port
 
-	port=$(( 4460 + ${1:-0} ))
-	export RMR_RTG_SVC=$(( 9990 + $1 ))
-	./receiver${si} $nmsg $port
-	echo $? >/tmp/PID$$.$1.rrc
+	port=4460
+	export RMR_RTG_SVC=9990
+	./receiver${si} $(( nmsg * nsenders ))  $port
+	echo $? >/tmp/PID$$.rrc
 }
 
-#	Drop a contrived route table in such that the sender sends each message to n
-#	receivers.
+#	Drop in a contrived route table. It should have only one entry which points
+#	message type 0 to the receiver.  The sender must NOT be defined for a valid
+#	tests (rts should not require the sendter be in the route thable).
 #
 function set_rt {
-	typeset port=4460
-	typeset groups="localhost:4460"
-	for (( i=1; i < ${1:-3}; i++ ))
-	do
-		groups="$groups;localhost:$((port+i))"
-	done
-
-	cat <<endKat >multi.rt
+	cat <<endKat >rts.rt
 		newrt | start
-		mse |0 | 0 | $groups
-		mse |1 | 10 | $groups
-		mse |2 | 20 | $groups
-		rte |3 | $groups
-		rte |4 | $groups
-		rte |5 | $groups
-		rte |6 | $groups
-		rte |7 | $groups
-		rte |8 | $groups
-		rte |9 | $groups
-		rte |10 | $groups
-		rte |11 | $groups
+		mse |5 | -1 | localhost:4460
 		newrt | end
 endKat
 
@@ -91,39 +71,31 @@ endKat
 
 # ---------------------------------------------------------
 
-if [[ ! -f local.rt ]]		# we need the real host name in the local.rt; build one from mask if not there
-then
-	hn=$(hostname)
-	sed "s!%%hostname%%!$hn!" rt.mask >local.rt
-fi
-
 nmsg=10						# total number of messages to be exchanged (-n value changes)
 delay=1000000				# microsec sleep between msg 1,000,000 == 1s
 wait=1
 rebuild=0
 nopull=""
 verbose=0
-nrcvrs=3					# this is sane, but -r allows it to be set up
-force_make=0
+nsenders=3					# this is sane, but -s allows it to be set up
 si=""
 
 while [[ $1 == -* ]]
 do
 	case $1 in
 		-B)	rebuild=1;;
-		-b)	rebuild=1; nopull="nopull";;		# enable build but without pull
+		-b)	rebuild=1; nopull="nopull";;	# build without pulling
 		-d)	delay=$2; shift;;
 		-n)	nmsg=$2; shift;;
-		-N)	si="";;								# buld/run NNG binaries (turn off si)
-		-M)	force_make=1;;
-		-r)	nrcvrs=$2; shift;;
-		-S)	si="_si";;							# buld/run SI95 binaries
+		-N) si="";;							# enable NNG tests (disable si)
+		-s)	nsenders=$2; shift;;
+		-S) si="_si";;						# enable SI95 tests
 		-v)	verbose=1;;
 
 		*)	echo "unrecognised option: $1"
-			echo "usage: $0 [-B] [-d micor-sec-delay] [-M] [-n num-msgs] [-S]"
-			echo "  -B forces an RMR rebuild which will use .build"
-			echo "  -m force test applications to be remade"
+			echo "usage: $0 [-B] [-d micor-sec-delay] [-M] [-n num-msgs] [-s nsenders] [-S]"
+			echo "  -B forces a rebuild which will use .build"
+			echo "  -M force test applications to be remade"
 			echo "  -S build/test SI95 based binaries"
 			exit 1
 			;;
@@ -154,44 +126,48 @@ else
 	fi
 fi
 
-if [[ -d $build_path/lib64 ]]
+if [[ -z LD_LIBRARY_PATH ]]			# honour if cmake test sets it
 then
-	export LD_LIBRARY_PATH=$build_path:$build_path/lib64:$LD_LIBRARY_PATH
-else
-	export LD_LIBRARY_PATH=$build_path:$build_path/lib:$LD_LIBRARY_PATH
+	if [[ -d $build_path/lib64 ]]
+	then
+		export LD_LIBRARY_PATH=$build_path:$build_path/lib64:$LD_LIBRARY_PATH
+	else
+		export LD_LIBRARY_PATH=$build_path:$build_path/lib:$LD_LIBRARY_PATH
+	fi
 fi
+
 export LIBRARY_PATH=$LD_LIBRARY_PATH
-export RMR_SEED_RT=./multi.rt
+export RMR_SEED_RT=./rts.rt
 
-set_rt $nrcvrs						# set up the rt for n receivers
+set_rt 		# create the route table
 
-if (( rebuild || force_make )) || [[ ! -f ./sender${si} || ! -f ./receiver${si} ]]
+if [[ ! -f ./sender${si} || ! -f ./receiver${si} ]]
 then
-	if ! make >/dev/null 2>&1
+	if ! make ./sender${si} ./receiver${si} >/dev/null 2>&1
 	then
 		echo "[FAIL] cannot find sender${si} and/or receiver${si} binary, and cannot make them.... humm?"
 		exit 1
 	fi
 fi
 
-for (( i=0; i < nrcvrs; i++ ))		# start the receivers with an instance number
-do
-	run_rcvr $i &
-done
+run_rcvr &
 
 sleep 2				# let receivers init so we don't shoot at an empty target
-run_sender &
+for (( i=0; i < nsenders; i++ ))		# start the receivers with an instance number
+do
+	run_sender $i &
+done
 
 wait
 
 
-for (( i=0; i < nrcvrs; i++ ))		# collect return codes
+for (( i=0; i < nsenders; i++ ))		# collect return codes
 do
-	head -1 /tmp/PID$$.$i.rrc | read x
-	(( rrc += x ))
+	head -1 /tmp/PID$$.$i.src | read x
+	(( src += x ))
 done
 
-head -1 /tmp/PID$$.src | read src
+head -1 /tmp/PID$$.rrc | read rrc
 
 if (( !! (src + rrc) ))
 then
---
+++ test/run_app_tests.sh
@@ -0,0 +1,69 @@
+#!/usr/env bash
+# vim: ts=4 sw=4 noet :
+#==================================================================================
+#    Copyright (c) 2020 Nokia
+#    Copyright (c) 2020 AT&T Intellectual Property.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#==================================================================================
+#
+
+# this is a simple wrapper until we figure out why CMake seems unable to start
+# the run_all script in the app test directory. Even after that it will likely
+# be needed because it seems impossible to set environment vars from CMake for
+# the test script which makes testing alternate install locations impossible.
+# So, we must jump some hoops to allow for that...
+
+# Hoop:
+# It seems that we cannot supply env vars on the CMake generated command :(
+# To deal with this, all leading positional parms of the form foo=bar are
+# looked at and if we like it we'll export it before starting the test.
+
+# It is also impossible for us to know what the build directory the user
+# created for their build and test. We will auto discover if they used
+# .build or build in the parent to this directory (in that order). If a
+# different directory is desired, then the build directory must be supplied
+# as an environment variable to the make:
+#	cd rmr/.bld
+#	BUILD_PATH=$PWD make test ARGS=-v
+
+while [[ $1 == *"="* ]]
+do
+	case ${1%%=*} in
+		CMBUILD)						# should be cmake build dir
+			if [[ -z $BUILD_PATH ]]		# still allow user to override
+			then
+				export BUILD_PATH=${1##*=}
+			fi
+			;;
+
+		LD_LIBRARY_PATH)
+			export LD_LIBRARY_PATH=${1##*=}
+			;;
+
+		C_INCLUDE_PATH)
+			export C_INCLUDE_PATH=${1##*=}
+			;;
+
+		LIBRARY_PATH)
+			export LIBRARY_PATH=${1##*=}
+			;;
+	esac
+
+	shift
+done
+
+
+set -e
+cd app_test
+bash ./run_all.ksh.sh  -S	# build CI likely doesn't have ksh; Run SI tests
