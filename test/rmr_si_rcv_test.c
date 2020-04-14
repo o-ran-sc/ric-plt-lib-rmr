e15b1382dc618f6832957e67dc638b5e8f3f414d
E. Scott Daniels <daniels@research.att.com>
2020-04-14 15:55:13 -0400

    Fix rmr_call() parameter checking bug
    When RMR was extended to add wormhole support for
    a rmr_wh_call() function, the main call code was broken
    into an inward facing funciton.  A parameter check which
    applies only to the outward facing rmr_call() API function
    was accidentally moved to the internal funciton.  This change
    moves the check back into the rmr_call() funciton.
    
    Issue-ID: RIC-333
    
    Signed-off-by: E. Scott Daniels <daniels@research.att.com>
    Change-Id: Iab0c8cc33e8a9970c53b9248021a1273559f82f8

---
+++ test/rmr_si_rcv_test.c
@@ -0,0 +1,101 @@
+// :vi sw=4 ts=4 noet:
+/*
+==================================================================================
+	Copyright (c) 2020 Nokia
+	Copyright (c) 2020 AT&T Intellectual Property.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================================
+*/
+
+/*
+	Mmemonic:	rmr_si_rcv_test.c
+	Abstract:	This drives only the receive tests for the SI API. Because
+				of the threaded nature of SI receives it is not possible to
+				mix these tests with the other coverage tests which allocate
+				various contexes.
+
+	Author:		E. Scott Daniels
+	Date:		14 April 2020		(AKD)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <errno.h>
+#include <string.h>
+#include <errno.h>
+#include <pthread.h>
+#include <ctype.h>
+
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <errno.h>
+#include <string.h>
+#include <stdint.h>
+#include <ctype.h>
+#include <sys/epoll.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define DEBUG 1
+#define PARANOID_CHECKS	1					// must have parinoid testing on to not fail on nil pointer tests
+
+											// specific test tools in this directory
+#undef NNG_UNDER_TEST 
+#include "test_support.c"					// things like fail_if()
+#include "test_ctx_support.c"				// dummy context support
+#include "test_msg_support.c"
+#include "test_gen_rt.c"
+
+
+#include "rmr.h"					// things the users see
+#include "rmr_symtab.h"
+#include "rmr_logging.h"
+#include "rmr_agnostic.h"			// transport agnostic header
+
+#include "symtab.c"
+#include "logging.c"
+#include "rmr_si.c"
+#include "mbuf_api.c"
+
+
+static void gen_rt( uta_ctx_t* ctx );		// defined in sr_si_static_test, but used by a few others (eliminate order requirement below)
+
+											// and finally....
+#include "rmr_si_rcv_static_test.c"			// the only test driver
+
+
+/*
+	Drive each of the separate tests and report.
+*/
+int main() {
+	int errors = 0;
+
+	rmr_set_vlevel( 5 );			// enable all debugging
+
+	fprintf( stderr, "\n<INFO> starting receive tests (%d)\n", errors );
+	errors += rmr_rcv_test();
+	fprintf( stderr, "<INFO> error count: %d\n", errors );
+
+	if( errors == 0 ) {
+		fprintf( stderr, "<PASS> all tests were OK\n\n" );
+	} else {
+		fprintf( stderr, "<FAIL> %d modules reported errors\n\n", errors );
+	}
+
+	return !!errors;
+}
---
+++ test/test_msg_support.c
@@ -0,0 +1,94 @@
+// vi: ts=4 sw=4 noet :
+/*
+==================================================================================
+	    Copyright (c) 2019-2020 Nokia
+	    Copyright (c) 2018-2020 AT&T Intellectual Property.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================================
+*/
+
+/*
+	Mnemonic:	test_msg_support.c
+	Abstract:	Support for testing with messages; requires RMR defs, so not
+				to be included for things like ring tests etc.
+	Author:		E. Scott Daniels
+	Date:		14 April 2020	(AKD)
+*/
+
+#ifndef _test_msg_support_c
+#define _test_msg_support_c
+
+#include <signal.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int test_support_xact_count = 0;
+/*
+	Send a 'burst' of messages to drive some send retry failures to increase RMr coverage
+	by handling the retry caee.
+*/
+static void send_n_msgs( void* ctx, int n ) {
+	rmr_mbuf_t*	msg;			// message buffers
+	int i;
+
+	msg = rmr_alloc_msg( ctx,  1024 );
+	if( ! msg ) {
+		fprintf( stderr, "<FAIL> mass send of %d messages couldn't allocate message!\n", n );
+		return;
+	}
+
+	fprintf( stderr, "<INFO> mass send of %d messages\n", n );
+	for( i = 0; i < n; i++ ) {
+		msg->len = 100;
+		msg->mtype = 1;
+		msg->state = 999;
+
+		snprintf( msg->xaction, 32, "%015d", test_support_xact_count++ );		// simple transaction id so we can test receive specific and ring stuff
+
+		errno = 999;
+		msg = rmr_send_msg( ctx, msg );
+		if( msg && msg->state != 0 ) {
+			fprintf( stderr, "<WARN> mass send failed: state=%d type=%d\n", msg->state, msg->mtype );
+		}
+	}
+}
+
+
+/*
+	Allow test to reset the transaction id counter.
+*/
+static void reset_xact_count() {
+	test_support_xact_count = 0;
+}
+
+/*
+	Refresh or allocate a message with some default values
+*/
+static rmr_mbuf_t* fresh_msg( void* ctx, rmr_mbuf_t* msg ) {
+	if( ! msg )  {
+		msg = rmr_alloc_msg( ctx, 2048 );
+	}
+
+	msg->mtype = 0;
+	msg->sub_id = -1;
+	msg->state = 0;
+	msg->len = 100;
+
+	return msg;
+}
+
+#endif
